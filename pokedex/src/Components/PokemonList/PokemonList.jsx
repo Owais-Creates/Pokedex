1ce17d5d8b87a32725c220ddaf472e802c561d2d
Owais <zaidalit00@gmail.com>
2024-03-16 21:13:18 +0530

    added pagination

--- pokedex/src/Components/PokemonList/PokemonList.jsx
+++ pokedex/src/Components/PokemonList/PokemonList.jsx
@@ -8,11 +8,15 @@ const PokemonList = () => {
     let pokemon;
     const [isLoading, setIsLoading] = useState(true);
     const [pokemonList, setPokemonList] = useState([]);
-    const POKEDEX_API = 'https://pokeapi.co/api/v2/pokemon'
+    const [nextUrl, setNextUrl] = useState("")
+    const [prevUrl, setPrevUrl] = useState("")
+    const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
 
     const downloadData = async () => {
-        const res = await axios.get(POKEDEX_API);
+        const res = await axios.get(pokedexUrl);
         const pokemonResults = res.data.results;
+        setNextUrl(res.data.next);
+        setPrevUrl(res.data.previous);
 
         const pokemonData = pokemonResults.map((p) => axios.get(p.url));
         const pokemonDataResults = await axios.all(pokemonData)
@@ -26,20 +30,19 @@ const PokemonList = () => {
                 id: pokemon.id
             }
         })
-        console.log(neededPokemonData);
         setPokemonList(neededPokemonData)
         setIsLoading(false);
     }
 
     useEffect(() => {
         downloadData()
-    }, [])
+    }, [pokedexUrl])
 
 
     return (
         <>
             <div className="pokemon-list-wrapper" >
-                <p>PokemonList</p>
+                <h1>Pokemon List</h1>
                 {isLoading ? "Loading....." :
 
                     <div className="pokemon-card-wrapper" >
@@ -50,6 +53,11 @@ const PokemonList = () => {
                     </div>
 
                 }
+            </div>
+
+            <div className="controls" >
+                <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)} >PREVIOUS</button>
+                <button  disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)} >NEXT</button>
             </div>
         </>
     )
